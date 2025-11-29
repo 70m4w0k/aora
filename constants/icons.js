14c351fa1f33889a7b0fd33bc6f910e7ee2d5cbf
tomawok dev <tomawok.dev@protonmail.com>
2025-11-29 19:16:52 +0100

    chore: clean up project for roommate app
    Remove unused features (plants tracker, videos) to prepare for roommate management app.
    
    Removed:
    - Plants tracker components (GardenCalendar, PlantCard, CreatePlantModal, CreatePlantEventModal)
    - Video-related functions from appwrite.js
    - All plant-related functions from appwrite.js
    - Unused icons (play, bookmark, seed-outline)
    - Unused images (thumbnail, cards, path, screenshots)
    
    Updated:
    - Calendar tab now shows only chores (removed garden toggle)
    - Simplified appwrite.js config (removed unused collection IDs)
    - Fixed app.json schema (removed invalid 'debug' property)
    - Added expo.doctor exclusions for package warnings
    
    Kept features:
    - User authentication
    - Chores/tasks tracker
    - Shopping list
    - Expense sharing
    
    Project now on Expo SDK 52 with React 18.3.1

--- constants/icons.js
+++ constants/icons.js
@@ -1,4 +1,3 @@
-import bookmark from "../assets/icons/bookmark.png";
 import home from "../assets/icons/home.png";
 import plus from "../assets/icons/plus.png";
 import profile from "../assets/icons/profile.png";
@@ -10,16 +9,12 @@ import rightArrow from "../assets/icons/right-arrow.png";
 import logout from "../assets/icons/logout.png";
 import eyeHide from "../assets/icons/eye-hide.png";
 import eye from "../assets/icons/eye.png";
-import play from "../assets/icons/play.png";
 import calendar from "../assets/icons/calendar.png";
 import list from "../assets/icons/list.png";
 import calculator from "../assets/icons/calculator.png";
 import tipi from "../assets/icons/tipi.png";
-import seedOutline from "../assets/icons/seed-outline.png";
 
 export default {
-  play,
-  bookmark,
   home,
   plus,
   profile,
@@ -31,9 +26,9 @@ export default {
   logout,
   eyeHide,
   eye,
+  "eye-hide": eyeHide,
   calendar,
   list,
   calculator,
   tipi,
-  seedOutline,
 };
